02464ed1f0bf1c279b2322b3a0c7addfa1700cc9
sandip Kachhadiya <sandeep.scet@gmail.com>
2023-03-30 18:35:02 +0530

    feature header

--- src/Component/Subcription/index.js
+++ src/Component/Subcription/index.js
@@ -1,14 +1,39 @@
 import { Box, Button, Typography } from '@mui/material'
 import { Colors } from '@/src/Theme/colors'
 import { typography } from '@/src/Theme/typography'
+import React from 'react'
+
 const Subscription = () => {
+  const [hydrated, setHydrated] = React.useState(false)
+  React.useEffect(() => {
+    setHydrated(true)
+  }, [])
+
+  const featureActions = [
+    {
+      text: 'Want Updates of New Prompts and Features? No Spam.',
+      button: (
+        <a target="_blank" href="https://github.com/sandeepscet/prompt-apps/">
+          <Button variant="secondary" style={{ ...typography.body12Regular, color: Colors.White }}>
+            Subscribe
+          </Button>
+        </a>
+      ),
+    },
+    {
+      text: 'Free. No Sign-Up Required. No Limits.',
+    },
+  ]
+
+  const feature = featureActions[Math.floor(Math.random() * featureActions.length)]
+
   return (
     <Box
       sx={{
         width: '100%',
         // height:50,
-        
-        justifyContent:'center',
+
+        justifyContent: 'center',
         backgroundColor: Colors.Color14,
         '&:hover': {
           backgroundColor: Colors.Color14,
@@ -21,11 +46,18 @@ const Subscription = () => {
         variant="body2"
         color="text.secondary"
         align="center"
-        style={{ ...typography.body18Regular,color:Colors.White ,paddingTop:10,paddingBottom:10}}
-        onClick={() => alert('fdgderf')}
+        style={{
+          ...typography.body18Regular,
+          color: Colors.White,
+          paddingTop: 10,
+          paddingBottom: 10,
+        }}
       >
-        Features Prompts
-        <Button  style={{ ...typography.body12Regular,color:Colors.White}}>Subscribe</Button>
+        {hydrated && feature['text']}
+        {hydrated && feature['button']}
+
+        {!hydrated && featureActions[0]['text']}
+        {!hydrated && featureActions[0]['button']}
       </Typography>
     </Box>
   )
