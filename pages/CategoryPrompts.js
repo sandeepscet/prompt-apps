00555bccec1fac1569928ef58bf0d8f9186356e7
sandip Kachhadiya <sandeep.scet@gmail.com>
2023-05-01 13:30:00 +0530

    prompt added in title for SEO

--- pages/CategoryPrompts.js
+++ pages/CategoryPrompts.js
@@ -24,8 +24,12 @@ const CategoriesPrompt = () => {
 	return (
 		<>
 			<Head>
-				<title>{`${categoryId  } Prompt Apps`}</title>
-				<meta name="description" content="{category.description}" key="desc" />
+				<title>{`${categoryId} Prompt Apps`}</title>
+				<meta
+					name="description"
+					content="{category.description} chatgpt Prompts"
+					key="desc"
+				/>
 				<meta name="viewport" content="width=device-width, initial-scale=0.1" />
 			</Head>
 			{categoryId && (
